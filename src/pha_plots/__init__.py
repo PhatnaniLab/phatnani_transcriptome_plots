9665578576db6b01a632560356898b3b26056993
CJACKSON <cj59@nyu.edu>
2026-06-04 10:24:50 -0400

    toplevel import

--- src/pha_plots/__init__.py
+++ src/pha_plots/__init__.py
@@ -7,4 +7,15 @@ try:
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "0.0.0.dev0"
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "draw_frontal_cortex",
+    "draw_motor_cortex",
+    "draw_spinal_cord"
+]
+
+from .spatial import (
+    draw_frontal_cortex,
+    draw_motor_cortex,
+    draw_spinal_cord
+)
